2d2a5d077968d89223ad8f65e8d855eb45fc0250
ralfstory <89399484+ralfstory@users.noreply.github.com>
2022-04-17 14:33:22 -0400

    okay finally

--- src/lib/code.svelte
+++ src/lib/code.svelte
@@ -37,54 +37,55 @@
 			.map(({ colorIndex, numberOfPixels }) => {
 				const paddedColor = colorIndex.toString(2).padStart(colorCompression, '0');
 				const paddedPixels = numberOfPixels.toString(2).padStart(pixelCompression, '0');
-				console.log({ paddedColor, paddedPixels });
 				return `${paddedColor}${paddedPixels}`;
 			})
 			.reduce((acc, curr) => `${curr}${acc}`);
 
-		console.log({ result });
 		return result;
 	};
 
 	const byteToHex = {
-		'0000': 0,
-		'0001': 1,
-		'0010': 2,
-		'0011': 3,
-		'0100': 4,
-		'0101': 5,
-		'0110': 6,
-		'0111': 7,
-		'1000': 8,
-		'1001': 9,
-		'1010': 10,
-		'1011': 11,
-		'1100': 12,
-		'1101': 13,
-		'1110': 14,
-		'1111': 15
+		'0000': '0',
+		'0001': '1',
+		'0010': '2',
+		'0011': '3',
+		'0100': '4',
+		'0101': '5',
+		'0110': '6',
+		'0111': '7',
+		'1000': '8',
+		'1001': '9',
+		'1010': 'a',
+		'1011': 'b',
+		'1100': 'c',
+		'1101': 'd',
+		'1110': 'e',
+		'1111': 'f'
 	};
 
 	const binaryToHex = (binary: string) => {
 		let paddedBinary = binary;
 		let converted = '';
-		let failSafe = 0;
 
-		while (paddedBinary.length % 4 || failSafe > 4) {
-			paddedBinary = paddedBinary.padStart(1, '0');
-			failSafe += 1;
+		// make sure we are divisible by 4 bits
+		while (paddedBinary.length % 4) {
+			paddedBinary = `0${paddedBinary}`;
 		}
 
 		for (let i = 0; i < paddedBinary.length / 4; i += 1) {
 			const end = paddedBinary.length - i * 4;
 			const byte = paddedBinary.slice(end - 4, end);
 
-			if (!byteToHex[byte]) {
+			console.log({ byte });
+
+			if (byteToHex[byte] == undefined) {
 				console.log('conversion failed: ', byte);
 			}
 
-			converted = byte + byteToHex[byte];
+			converted = `${byteToHex[byte]}${converted}`;
 		}
+
+		console.log(binary, converted);
 		return `0x${converted}`;
 	};
 
@@ -95,9 +96,9 @@
 		let binaryReduction = binary;
 
 		let processing = true;
-		let failSafe = 0;
+		let maxIterations = 0;
 
-		while (processing || failSafe > 10) {
+		while (processing && maxIterations < 256) {
 			const slice = binaryReduction.slice(binaryReduction.length - 256, binaryReduction.length);
 			binaryReduction = binaryReduction.slice(0, binaryReduction.length - 256);
 
@@ -109,7 +110,7 @@
 			}
 			uint256s.push(binaryToHex(slice));
 
-			failSafe += 1;
+			maxIterations += 1;
 		}
 
 		return uint256s;
@@ -121,6 +122,8 @@
 </script>
 
 <div>
+	{console.log('render')}
 	{JSON.stringify(packets)} total bits: {packets.length * (4 + 3)} naive: {colors.length * 3}
+	{JSON.stringify(packetsToBinary(packets, 4, 3))}
 	{JSON.stringify(binaryToUint256(packetsToBinary(packets, 4, 3)))}
 </div>
