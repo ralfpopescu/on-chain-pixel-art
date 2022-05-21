537ed2ca3638d53ccac473b18657bab929fab560
ralfstory <89399484+ralfstory@users.noreply.github.com>
2022-05-21 19:12:38 -0400

    remove dragging icon

--- src/lib/pixel.svelte
+++ src/lib/pixel.svelte
@@ -1,24 +1,36 @@
 <script lang="ts">
+	import { asDraggable } from 'svelte-drag-and-drop-actions';
 	export let color: string;
 	export let isSelected: boolean = false;
 	export let handleClick = () => null;
 	export let rounded = false;
+
+	const preventDefault = (handler: () => any) => (e: any) => {
+		e.preventDefault();
+		handler();
+	};
 </script>
 
 <div
-	style={`background-color: ${color}; ${
+	style={`background-color: ${color}; user-drag: none; ${
 		isSelected ? 'border: 2px solid #eeeeee;' : 'border: 1px dotted grey'
 	}`}
 	class={`pixel w-5 cursor-crosshair hover:opacity-90 ${rounded ? 'rounded-full' : ''}`}
-	on:click={handleClick}
-	on:dragover={handleClick}
+	on:click={preventDefault(handleClick)}
+	on:dragover={preventDefault(handleClick)}
 	on:dragstart={() => console.log('start')}
 	on:dragend={() => console.log('end')}
-	draggable={true}
+	use:asDraggable={{ Dummy: `<div style="opacity: 0; cursor: crosshair;">.</div>` }}
 />
 
 <style>
-	:global(#dnd-action-dragged-el .pixel) {
-		transform: rotate(7deg);
+	:global([draggable]) {
+		-webkit-touch-callout: none;
+		-ms-touch-action: none;
+		touch-action: none;
+		-moz-user-select: none;
+		-webkit-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
 	}
 </style>
